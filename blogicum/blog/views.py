7fb8dfc2b7e3af344aa93d362a0bf149b21d1615
flashikNet <133534020+flashikNet@users.noreply.github.com>
2024-11-09 17:19:44 +0500

    Переписал view функции

--- blogicum/blog/views.py
+++ blogicum/blog/views.py
@@ -1,20 +1,31 @@
 from django.http import Http404
 from django.shortcuts import render
+from .models import Post, Category
+from django.utils import timezone
 
 
 def index(request):
     template_name = 'blog/index.html'
+    posts = Post.objects.select_related(
+        "category", "location", "author"
+    ).filter(
+        pub_date__lte=timezone.now(),
+        is_published=True,
+        category__is_published=True
+    ).order_by("-pub_date")[:5]
     context = {
-        'posts': list(reversed(posts))
+        'post_list': posts
     }
     return render(request, template_name, context=context)
 
 
 def post_detail(request, id):
     template_name = 'blog/detail.html'
-    post = next(filter(lambda x: x['id'] == id, posts), None)
-    if post is None:
-        raise Http404(f'Post with id:{id} does not exist')
+    post = Post.objects.get(pk=id)
+    if (not post.category.is_published
+            or not post.is_published
+            or post.pub_date > timezone.now()):
+        raise Http404()
     context = {
         'post': post
     }
@@ -23,7 +34,18 @@ def post_detail(request, id):
 
 def category_posts(request, category_slug):
     template_name = 'blog/category.html'
+    category = Category.objects.get(slug=category_slug)
+    if category is None or not category.is_published:
+        raise Http404()
+    posts = Post.objects.select_related(
+        "category", "author", 'location'
+    ).filter(
+        category__id=category.pk,
+        is_published=True,
+        pub_date__lte=timezone.now()
+    )
     context = {
-        'category': category_slug
+        'category': category,
+        'post_list': posts
     }
     return render(request, template_name, context=context)
